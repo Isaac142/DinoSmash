34231f1d5d09c1e66bca6f27303ef12966695140
Isaac <embramaze@gmail.com>
2019-11-20 19:12:44 +1100

    Movement/Score

---
+++ DinoSmash/Assets/Scripts/DestroyVehicle.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DestroyVehicle : MonoBehaviour
+{
+
+    public Text scoreText;
+    public int score;
+
+    public Text timeText;
+    public float timer;
+
+    public bool isOver = false;
+    public GameObject isOverGO;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        score = 0;
+        SetScoreText();
+        timer = 30f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(timer < 5)
+        {
+            timeText.text = "Time: " + timer.ToString("F2");
+        }
+        else
+        {
+            timeText.text = "Time: " + timer.ToString("F0");
+        }
+
+        if(timer <= 0)
+        {
+            isOverGO.SetActive(true);
+            timer = 0f;
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+        if(isOver)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+        }
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+
+        if (col.gameObject.tag == "Car")
+        {
+            Destroy(col.gameObject);
+            score = score + 100;
+            SetScoreText();
+        }
+
+        if (col.gameObject.tag == "Truck")
+        {
+            Destroy(col.gameObject);
+            score = score + 125;
+            SetScoreText();
+        }
+
+        if (col.gameObject.tag == "Bike")
+        {
+            Destroy(col.gameObject);
+            score = score + 25;
+            SetScoreText();
+        }
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+}
---
+++ DinoSmash/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Transform leftPivot, rightPivot;
+    public float rotateSpeed;
+    bool canPivotLeft = true, canPivotRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKey(KeyCode.A) && canPivotLeft)
+        {
+            canPivotRight = false;
+            Vector3 pos = leftPivot.transform.position;
+            transform.Rotate(Vector3.down, rotateSpeed * Time.deltaTime, Space.Self);
+            pos -= leftPivot.transform.position;
+            transform.position += pos;
+        }
+
+        if (Input.GetKey(KeyCode.D) && canPivotRight)
+        {
+            canPivotLeft = false;
+            Vector3 pos = rightPivot.transform.position;
+            transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime, Space.Self);
+            pos -= rightPivot.transform.position;
+            transform.position += pos;
+        }
+
+        if(Input.GetKeyUp(KeyCode.A))
+        {
+            canPivotRight = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            canPivotLeft = true;
+        }
+
+    }
+}
